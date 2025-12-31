1ffa3353c08e6a424eb0dc29cea86369d7d52cc5
MohdShahulMalik <mohdshahulmalik@gmail.com>
2025-12-31 17:50:59 +0000

    Trying out a custom splash screen

--- causeconnect-mobile/app/_layout.tsx
+++ causeconnect-mobile/app/_layout.tsx
@@ -2,13 +2,23 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import * as BootSplash from 'react-native-bootsplash';
+import * as Font from 'expo-font';
+import { Animated, Appearance } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
 import 'react-native-reanimated';
 import "./main.css"
 import { Auth0Provider } from 'react-native-auth0';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import { Poppins_700Bold } from '@expo-google-fonts/poppins';
+
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,43 +31,74 @@ export const unstable_settings = {
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+  const [appReady, setAppReady] = useState<boolean>(false);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.8)).current;
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    async function prepare() {
+      await Font.loadAsync({
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_600SemiBold,
+        Inter_700Bold,
+        Poppins_700Bold,
+        ...FontAwesome.font
+      });
+
+      await new Promise(resolve => setTimeout(resolve, 1500));
+
+      setAppReady(true);
+    }
+  }, [])
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    if (!appReady) return;
 
-  if (!loaded) {
+    Animated.parallel([
+      Animated.timing(fadeAnim, { toValue: 1, duration: 800, useNativeDriver: true }),
+      Animated.spring(scaleAnim, { toValue: 1, tension: 120, friction: 8, useNativeDriver: true})
+    ]).start(async () => {
+      await BootSplash.hide({ fade: true });
+    });
+
+  }, [appReady]);
+
+  if (!appReady) {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return <RootLayoutNav fadeAnim = {fadeAnim} scaleAnim={scaleAnim}/>;
 }
 
-function RootLayoutNav() {
+interface RootLayoutNavProps {
+  fadeAnim: Animated.Value;
+  scaleAnim: Animated.Value;
+}
+
+function RootLayoutNav(props: RootLayoutNavProps) {
   const colorScheme = useColorScheme();
 
+  const fadeAnim = props.fadeAnim;
+  const scaleAnim = props.scaleAnim;
+
   return (
-    <Auth0Provider domain = "dev-zczlap588q6otywu.au.auth0.com" clientId = "bI8V73uWqe8dEuxHvrzmLiOdgFHWD9Rf">
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack>
-      </ThemeProvider>
-    </Auth0Provider>
+    <Animated.View style={{
+      flex: 1,
+      opacity: fadeAnim,
+      transform: [{ scale: scaleAnim }]
+    }}>
+      <Auth0Provider domain = "dev-zczlap588q6otywu.au.auth0.com" clientId = "bI8V73uWqe8dEuxHvrzmLiOdgFHWD9Rf">
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          </Stack>
+        </ThemeProvider>
+      </Auth0Provider>
+    </Animated.View>
   );
 }
